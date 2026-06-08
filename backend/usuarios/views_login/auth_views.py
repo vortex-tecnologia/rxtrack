b8a88603c05a86286ff6b11c07f589a01b022655
lgluiz1 <legalhints@gmail.com>
2026-06-08 17:40:46 -0300

    fix: adiciona imports faltantes no auth_views.py

--- backend/usuarios/views_login/auth_views.py
+++ backend/usuarios/views_login/auth_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
