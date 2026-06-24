2d5dd7bb0014a7042df8fe83397d3ef896ebf78a
lgluiz1 <legalhints@gmail.com>
2026-06-24 15:22:53 -0300

    chore: revert JWT settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -252,8 +252,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
 
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
