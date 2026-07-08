a89eb47f20990f7aa3942e2dbffdf2f6ae896aed
lgluiz1 <legalhints@gmail.com>
2026-07-08 09:01:36 -0300

    fix(migrations): merge migrations in manifesto to fix multiple leaf nodes

---
+++ backend/manifesto/migrations/0040_merge_fix.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('manifesto', '0038_webhookeventosoap'),
+        ('manifesto', '0039_merge_20260619_1029'),
+    ]
+
+    operations = [
+    ]
