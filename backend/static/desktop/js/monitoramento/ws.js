6a4f16925c140f76b2cef49c3fcae3282e2a9e5f
suporte@rdexp.com.br <suporte@rdexp.com.br>
2026-09-11 13:43:11 -0300

    fix(mapa): substitui tiles por esri world street map sem bloqueios nem marca dagua

--- backend/static/desktop/js/monitoramento/ws.js
+++ backend/static/desktop/js/monitoramento/ws.js
@@ -819,10 +819,9 @@ function initMapaRastreamento(lat, lng) {
 
     mapaRastreamento = L.map('mapa-rastreamento').setView([defaultLat, defaultLng], 15);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri',
+        maxZoom: 19
     }).addTo(mapaRastreamento);
 
     // Força o Leaflet a recalcular o tamanho do container (resolve problema de mapa cinza/cortado)
--- backend/static/js/roteirizacao_v1.js
+++ backend/static/js/roteirizacao_v1.js
@@ -246,10 +246,9 @@
             attributionControl: false
         }).setView([latIni, lngIni], 13);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 20
+        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri',
+            maxZoom: 19
         }).addTo(_mapaInstancia);
 
         L.control.zoom({ position: 'bottomright' }).addTo(_mapaInstancia);
