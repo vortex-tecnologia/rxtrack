04ad2201088bddcd5a4d170a8c72855eb94c3714
suporte@rdexp.com.br <suporte@rdexp.com.br>
2026-09-11 13:29:18 -0300

    fix(mapa): substitui tiles osm bloqueados 403 por carto voyager em todos os modais

--- backend/static/js/roteirizacao_v1.js
+++ backend/static/js/roteirizacao_v1.js
@@ -246,8 +246,10 @@
             attributionControl: false
         }).setView([latIni, lngIni], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 20
         }).addTo(_mapaInstancia);
 
         L.control.zoom({ position: 'bottomright' }).addTo(_mapaInstancia);
